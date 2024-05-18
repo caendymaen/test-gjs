21feb054ac9e99cabfce1069a0d00d0833e87d0b
caendymaen <grill.andreas@aon.at>
2024-05-18 09:33:44 +0200

    a

---
+++ nix/default.nix
@@ -0,0 +1,67 @@
+{ 
+  lib,
+  stdenv,
+  buildNpmPackage,
+  nodejs,
+  gobject-introspection,
+  gobject-introspection-unwrapped,
+  gtk3,
+  gtk4,
+  wrapGAppsHook,
+  gtk4-layer-shell,
+  autoPatchelfHook,
+  glib,
+  glibc,
+  python3,
+  cairo,
+  gjs
+}:
+
+stdenv.mkDerivation rec {
+  pname = "node-gtk-test";
+  version = "1";
+
+  src = ../.;
+
+  #src = ../.;
+ installPhase = ''
+    mkdir -p $out
+    cp -r ./ $out 
+  '';
+    #dontWrapGApps = true;
+
+  nativeBuildInputs = [
+    wrapGAppsHook
+    autoPatchelfHook
+    gobject-introspection
+    gobject-introspection-unwrapped
+    glib
+    glib.dev
+    gjs
+  ];
+
+  buildInputs = [
+    gtk3
+    gtk4
+    gtk4-layer-shell
+    nodejs
+    glib.dev
+    #glib
+    python3
+    cairo
+  ];
+
+  propagatedBuildInputs = [
+    #glib
+  ];
+
+  outputs = [ "out" ];
+
+  meta = with lib; {
+    description = "A customizable and extensible shell";
+    homepage = "https://example.com";
+    platforms = [ "x86_64-linux" ];
+    license = licenses.gpl3;
+    meta.maintainers = [ ];
+  };
+}
